0b5f9e2cb4a259aa95b67cee5e2af722fbd1b6ff
facufrnandz <facufrnandz@gmail.com>
2021-02-20 16:38:50 -0300

    Creacion de cronometro

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
+
+import React, {useState} from 'react';
 import './App.css';
 
+
+const Display = ({hours, minutes, seconds}) =>{
+
+  return(
+
+    <p>{hours}:{minutes}:{seconds}</p>
+  );
+}
+
+const Button = ({setTimer, text}) =>{
+
+  return(
+
+    <button onClick= {setTimer}>{text}</button>
+  );
+}
+
 function App() {
+
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  if (isActive){
+    if (seconds < 59){
+      setTimeout(()=>setSeconds(seconds + 1),200);
+    }
+    
+    if (seconds === 59){
+      setSeconds(0)
+      setTimeout(()=> setMinutes(minutes + 1), 200);
+    }
+    if (minutes === 59){
+      setMinutes(0);
+      setTimeout(()=> setHours(minutes + 1), 200);
+    }
+
+  }
+
+  const timerOn = () =>{
+    setIsActive(true);
+  }
+
+  const timerOff = () =>{
+    setIsActive(false);
+  }
+
+  const setToZero = () =>{
+    setTimeout(()=> setHours(0), 200);
+    setTimeout(()=> setMinutes(0), 200);
+    setTimeout(()=>setSeconds(0), 200);
+    timerOff();
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      
+      <Display hours={hours} minutes={minutes} seconds={seconds}/>
+      <Button setTimer = {timerOn} text = 'Iniciar'/>
+      <Button setTimer = {timerOff} text = 'Parar'/>
+      <Button setTimer = {setToZero} text = 'Reiniciar'/>
+     
     </div>
+    
   );
 }
 
